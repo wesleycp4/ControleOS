9ac61f91cf1f9c7a3eb248306f664da687663d14
Wesley Camargos <wesleycamargos@live.com>
2017-09-25 22:59:57 -0300

    ok

--- app/src/main/java/com/dev/wcp4/controleos/Conexao/Rotas.java
+++ app/src/main/java/com/dev/wcp4/controleos/Conexao/Rotas.java
@@ -3,7 +3,7 @@ package com.dev.wcp4.controleos.Conexao;
 public class Rotas {
     private static final String ROOT_URL = "http://192.168.1.250/ControleOS/SERVICOS/";
     public static final String CADASTRAR_USUARIO = ROOT_URL+"usuarioNovo.php";
-    //public static final String CADASTRAR_CLIENTE = ROOT_URL+"clienteNovo.php";
+    public static final String CADASTRAR_CLIENTE = ROOT_URL+"clienteNovo.php";
     public static final String URL_LOGIN = ROOT_URL+"usuarioLogin.php";
     //public static final String URL_DADOS_USUARIO = ROOT_URL+"userDados.php";
     //public static final String URL_UPDATE_USUARIO = ROOT_URL+"userUpdate.php";
