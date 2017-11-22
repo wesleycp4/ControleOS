bcff59d62af0b96d43e640d792232cc71ab707d5
Wesley <wesleycp4@gmail.com>
2017-11-22 19:50:21 -0200

    consultar cliente iniciado

---
+++ app/src/main/java/com/dev/wcp4/controleos/Activity/ConsultarClienteActivity.java
@@ -0,0 +1,15 @@
+package com.dev.wcp4.controleos.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dev.wcp4.controleos.R;
+
+public class ConsultarClienteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultar_cliente);
+    }
+}
