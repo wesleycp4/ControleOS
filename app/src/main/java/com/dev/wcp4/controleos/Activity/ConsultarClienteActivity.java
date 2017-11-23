71126ced0dabc7a487171f26909633a50aa9faee
Wesley <wesleycp4@gmail.com>
2017-11-22 23:36:19 -0200

    .

--- app/src/main/java/com/dev/wcp4/controleos/Activity/ConsultarClienteActivity.java
+++ app/src/main/java/com/dev/wcp4/controleos/Activity/ConsultarClienteActivity.java
@@ -11,6 +11,6 @@ public class ConsultarClienteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultar_cliente);
-        //stest
+        //stestgkjhg
     }
 }
